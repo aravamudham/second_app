e5c44c7df215d6e56dc4d9572527164879c5711f
Praveen Aravamudham <praveen.aravamudham@gmail.com>
2015-10-30 15:00:34 -0400

    fifth commit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+  	@title = "Sample App | Home"
   end
 
   def help
+  	@title = "Sample App | Help"
+  end
+
+  def about
+  	@title = "Sample App | About"
   end
 end
